ea713cf3b072b2c51fb373dde01f319d733b1393
tarak44 <tharak1298@gmail.com>
2026-02-07 11:30:58 +0530

    Format data package init

--- src/wildfire_mlops/data/__init__.py
+++ src/wildfire_mlops/data/__init__.py
@@ -1,4 +1,5 @@
 """Data loading utilities."""
+
 from .loader import DataConfig, build_dataloaders, build_transforms, save_class_names
 
 __all__ = ["DataConfig", "build_dataloaders", "build_transforms", "save_class_names"]
